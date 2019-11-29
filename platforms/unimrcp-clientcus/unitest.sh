c801e5c00e0cbe9fcea6fa97fddbe05c415945bd
charlie <daad777@126.com>
2019-11-29 17:03:48 +0800

    意涵一江水

--- platforms/unimrcp-clientcus/unitest.sh
+++ platforms/unimrcp-clientcus/unitest.sh
@@ -2,9 +2,9 @@
 #test help
 ./unimrcptool.sh
 #test Err
-./unimrcptool.sh synth2 nlsmsc0 siqi test.txt example.wav
-./unimrcptool.sh synth nlsmsc02 siqi test.txt example.wav
-./unimrcptool.sh synth nlsmsc0 siqi test2.txt example.wav
-./unimrcptool.sh synth nlsmsc0 siqi test.txt example.wav2
+./unimrcptool.sh synth2 nlsmsc0 siqi example.txt example.wav
+./unimrcptool.sh synth nlsmsc02 siqi example.txt example.wav
+./unimrcptool.sh synth nlsmsc0 siqi example2.txt example.wav
+./unimrcptool.sh synth nlsmsc0 siqi example.txt example.wav2
 #test Suc
-./unimrcptool.sh synth nlsmsc0 siqi test.txt example.wav
+./unimrcptool.sh synth nlsmsc0 siqi example.txt example.wav
